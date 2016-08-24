99edb27322cfe81e33838fd0c7e06b91730805e0
Leva7 <leva181777@gmail.com>
2016-08-24 12:14:03 +0000

    :bug: Fixes tests

--- server/test_request_handler.py
+++ server/test_request_handler.py
@@ -9,7 +9,7 @@ class RHTest(unittest.TestCase):
     rh = RequestHandler()
 
     def test_unpack_req(self):
-        req1 = self.rh.set_image_code + ',"1","2","3",'.encode() + b'4'
+        req1 = self.rh.set_image_code + b',"1","2","3",' + b'4'
         act1 = self.rh.unpack_req(req1)
         exp1 = (cc.set_image.value, ['1', '2', '3', b'4'])
         self.assertTupleEqual(act1, exp1)
@@ -20,9 +20,9 @@ class RHTest(unittest.TestCase):
         self.assertTupleEqual(act2, exp2)
 
     def test_unpack_resp(self):
-        resp1 = self.rh.profile_info_code + b',"1","2",3,"4",img'
+        resp1 = self.rh.profile_info_code + b',"request_id","1","2",3,"4",img'
         act1 = self.rh.unpack_resp(resp1)
-        exp1 = (sc.profile_info.value, ['1', '2', 3, '4', b'img'])
+        exp1 = (sc.profile_info.value, ['request_id', '1', '2', 3, '4', b'img'])
         self.assertTupleEqual(act1, exp1)
 
         resp2 = b'0,"1",["2","3","4",[]]'
